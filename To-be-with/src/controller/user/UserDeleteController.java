278d36ca72307a7c7b454e4a5cd49d099ac10132
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2019-05-16 22:41:45 +0900

    기본 구조

--- To-be-with/src/controller/user/UserDeleteController.java
+++ To-be-with/src/controller/user/UserDeleteController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
 
-public class UserDelelteController implements Controller{
+public class UserDeleteController implements Controller{
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
