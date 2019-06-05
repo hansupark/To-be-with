a88d60648468e3140f4443d7f97557c2a2af65c2
vltm100 <vltm100@naver.com>
2019-06-05 21:00:32 +0900

    Add files via upload

---
+++ To-be-with/src/controller/travel/TravelInsertController.java
@@ -0,0 +1,53 @@
+package controller.travel;
+
+
+import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import controller.HttpUtil;
+import service.TravelService;
+import vo.TravelVo;
+import dao.TravelDao;
+
+	public class TravelInsertController implements Controller {
+		
+		public void execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			req.setCharacterEncoding("UTF-8");
+			res.setContentType("text/html; charset = UTF-8");	
+           
+			String title = req.getParameter("title");
+		    String content = req.getParameter("content");
+		    String region = req.getParameter("region");
+		    String country = req.getParameter("country");
+		    
+		    
+	       TravelDao Dao = TravelDao.getInstance();
+		   TravelVo TravelVo = new TravelVo();
+	  
+		    TravelVo.setTitle(title);
+	        TravelVo.setContent(content);
+	        TravelVo.setRegion(region);
+	        TravelVo.setCountry(country);
+	        
+		  //TravelDao.setId(sessionID);
+         TravelService service = TravelService.getService();
+         int result = service.insertTravel(TravelVo);
+		System.out.println(result);
+		//res.sendRedirect("reseultList.do");
+		
+	
+		HttpUtil.forward(req,res,"resultList.jsp");
+	}
+	
+}
+
